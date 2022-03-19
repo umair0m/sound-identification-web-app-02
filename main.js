6658c2b728bda49b94fe900819fcf6ff193d891f
umair0m <101636248+umair0m@users.noreply.github.com>
2022-03-19 16:46:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier("", modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
